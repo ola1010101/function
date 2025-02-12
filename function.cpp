5a2635005b16d1d98c02f37d000a625498231dc0
Huawei <Huawei@DESKTOP-Q0CEIR0>
2025-02-12 22:31:34 +0500

    Добавьте файлы проекта.

---
+++ function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+
+template < typename peremena1, typename peremena2, typename masiv >
+masiv summa(peremena1 num1, peremena2 num2)
+{
+	sum = 0
+		sum = num1 + num2;
+	return sum;
+
+}
+
+
+template < typename peremena1, typename peremena2, typename masiv >
+masiv proizvedenie(peremena1 num1, peremena2 num2)
+{
+	pro = 0;
+	for (peremena2 i = 0; i <= num2; i++)
+	{
+		pro = num1 + num1;
+	}
+	return pro
+}
